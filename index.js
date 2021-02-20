2593ddc57777d2c3305ffe2388b2a3d2d4d82082
bartekwk2 <bbartek.wk@gmail.com>
2021-02-20 15:38:57 +0100

    ss

--- index.js
+++ index.js
@@ -261,6 +261,9 @@ const io = socketIO(server)
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on("send_message", (data) => {
+    socket.emit("receive_message", data)
+})
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
