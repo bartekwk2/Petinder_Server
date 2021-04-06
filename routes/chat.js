68875092e8eaa5c67b67a81ad66acdb1b6fd0a89
bartekwk2 <bbartek.wk@gmail.com>
2021-04-06 12:48:34 +0200

    asa'

--- routes/chat.js
+++ routes/chat.js
@@ -1,35 +1,36 @@
 const Individaul = require('../models/chat/individualConversation')
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+
 
 
 router.get('/getMessages',async (req,res)=>{ 
 
     const { userSenderID, userReceiverID,} = req.query;
     try{
-        let conversation = await Individaul
-        .find({users : {$all : [userSenderID, userReceiverID]}},
-            async (err, results)=> {
-                if(!results.length){
-                    let newConversation = new Individaul({
-                        users : [userSenderID,userReceiverID]
-                    })
-                    await newConversation.save();
-                    return res.status(200).json({
-                      success: true,
-                      conversation: newConversation,
-                      newConv : true,
-                    });
-
-                }
+        let conversation = await Individaul.aggregate([
+            { $match: {users : {$all : [mongoose.Types.ObjectId(userSenderID),mongoose.Types.ObjectId(userReceiverID)]}}},
+            { "$project": { "users":1,"messages": {$slice : [{$reverseArray:"$messages"},0,2]}
+             }} 
+        ])
+        if(!conversation.length){
+            let newConversation = new Individaul({
+                users : [userSenderID,userReceiverID]
             })
-            .slice('messages',-15)
-
+            await newConversation.save();
+            return res.status(200).json({
+              success: true,
+              conversation: newConversation,
+              newConv : true,
+            });
+        }else{
             res.status(200).json({
                 success: true,
                 conversation: conversation,
                 newConv : false,
             })
+        }
         }catch(err){
                 console.log(err.message);
                 res.status(500).json({
