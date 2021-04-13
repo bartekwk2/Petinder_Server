7a5b76970cf61889d02437d193ce79559a3812c8
bartekwk2 <bbartek.wk@gmail.com>
2021-04-13 15:38:53 +0200

    saas

--- index.js
+++ index.js
@@ -362,7 +362,7 @@ io.on('connection', socket => {
         dateOfSend : dateOfSend
       }
 
-      sendNotif(content,receiverChatID)
+      sendNotif(content,senderChatID)
 
     await Individaul
       .findOneAndUpdate(
